5465ab28e7b6e797f296d1fc4731a0ac1fbf72c2
Ricardo Ventura <ricardo_ventura@heartcore.co.jp>
2023-07-13 10:46:37 +0900

    everything until quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(cost:i32) -> i32 {
+    if(cost > 40){
+        cost * 1
+    }else{
+        cost * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
