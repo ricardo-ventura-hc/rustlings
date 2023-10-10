de43ac0ff23ec16b0f3d2186f757adfaa54fd3bf
Ricardo Ventura <ricardo_ventura@heartcore.co.jp>
2023-10-10 11:58:15 +0900

    Added a space

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -9,7 +9,7 @@ enum Message {
     // TODO: define the different variants used below
     Move { x: i32, y: i32 },
     Echo(String),
-    ChangeColor(i32, i32, i32),
+    ChangeColor(i32, i32, i32), 
     Quit,
 }
 
